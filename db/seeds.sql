e490797b5c87f3b021aea2190ee367dbdb4d3d0d
Linette Hsu <linette.hsu13@gmail.com>
2017-05-12 02:06:35 -0500

    adding styled files

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,6 +4,9 @@ INSERT INTO burgers (burger_name) VALUES ("In-n-Out Burger");
 
 SELECT * from burgers;
 
-UPDATE burgers SET devoured=false WHERE id=3;
+UPDATE burgers SET devoured=false WHERE id=1;
+
+DELETE FROM  burgers WHERE id=9;
 
 UPDATE burgers SET burger_name="Bacon Double Cheeseburger" WHERE id=1;
+UPDATE burgers SET burger_name="Venison Burger" WHERE id=2;
