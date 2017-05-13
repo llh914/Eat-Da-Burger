00047a3400512f6f26b8352df49621f9610355bd
Linette Hsu <linette.hsu13@gmail.com>
2017-05-12 22:14:14 -0500

    fix seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,10 +3,3 @@ INSERT INTO burgers (burger_name) VALUES ("venison burger");
 INSERT INTO burgers (burger_name) VALUES ("In-n-Out Burger");
 
 SELECT * from burgers;
-
-UPDATE burgers SET devoured=false WHERE id=1;
-
-DELETE FROM  burgers WHERE id=9;
-
-UPDATE burgers SET burger_name="Bacon Double Cheeseburger" WHERE id=1;
-UPDATE burgers SET burger_name="Venison Burger" WHERE id=2;
